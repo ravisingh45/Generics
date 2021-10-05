03869e7e1681b30a0a06a61433f8573bb9f922ec
ravisingh45@gmail.com <81140145+ravisingh45@users.noreply.github.com>
2021-10-06 01:58:27 +0530

    ADDED:Finding max number of float

--- src/main/java/com/bridgelabz/MaxNumber.java
+++ src/main/java/com/bridgelabz/MaxNumber.java
@@ -6,6 +6,12 @@ public class MaxNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter the three numbers:");
+        Float FirstNumber = scanner.nextFloat();
+        Float secondNumber = scanner.nextFloat();
+        Float thirdNumber = scanner.nextFloat();
+        Float maxNumber = comparesion(FirstNumber,secondNumber,thirdNumber);
+        System.out.println("the Maximum value is : "+maxNumber);
+        System.out.println("enter the three numbers:");
         Integer firstNumber = scanner.nextInt();
         Integer SecondNumber = scanner.nextInt();
         Integer ThirdNumber = scanner.nextInt();
@@ -22,5 +28,15 @@ public class MaxNumber {
        max = thirdNumber;
    }
    return max;
+    }
+    private static Float comparesion(Float firstNumber, Float secondNumber, Float thirdNumber) {
+        Float max = firstNumber;
+        if(secondNumber.compareTo(max)>0){
+            max = secondNumber;
+        }
+        if (thirdNumber.compareTo(max)>0){
+            max = thirdNumber;
+        }
+        return max;
     }
 }
