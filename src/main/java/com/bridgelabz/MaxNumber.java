6408ee7211e5579572d4e5e3bd5b0dfde94903d4
ravisingh45@gmail.com <81140145+ravisingh45@users.noreply.github.com>
2021-10-06 22:20:10 +0530

    ADDED:Take 3 different variable

--- src/main/java/com/bridgelabz/MaxNumber.java
+++ src/main/java/com/bridgelabz/MaxNumber.java
@@ -1,19 +1,54 @@
 package com.bridgelabz;
 
-public class MaxNumber {
-    public static <T extends Comparable> T max(T... elements) {
-        T max = elements[0];
-        for (T element : elements) {
-            if (element.compareTo(max) > 0) {
-                max = element;
-            }
+import java.util.Scanner;
+
+public class MaxNumber<E extends Comparable> {
+    E x, y, z;
+
+    public MaxNumber(E x, E y, E z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public static <T extends Comparable> T comparesion(T x, T y, T z) {
+        T max = x;
+        if (y.compareTo(max) > 0) {
+            max = y;
+        }
+        if (z.compareTo(max) > 0) {
+            max = z;
         }
         return max;
     }
     public static void main(String[] args) {
-        System.out.println("Integer Max: " + max(234,12,567,34,234));
-        System.out.println("Double Max: " + max(1.2,5.6,7.8));
-        System.out.println("String Max: " + max("ravi","ramesh","suresh","dilip","chinni","rajesh"));
+        Scanner scanner = new Scanner(System.in);
+        //float value from user
+        System.out.println("enter the float value:");
+        Float firstNumber = scanner.nextFloat();
+        Float secondNumber = scanner.nextFloat();
+        Float thirdNumber = scanner.nextFloat();
 
+        Float maxNumber = comparesion(firstNumber, secondNumber, thirdNumber);
+        System.out.println("Maximum value is:" + maxNumber);
+        System.out.println(" ");
+        //integer value form user
+        System.out.println("enter the integer value:");
+        Integer firstNumbers = scanner.nextInt();
+        Integer secondNumbers = scanner.nextInt();
+        Integer thirdNumbers = scanner.nextInt();
+
+        Integer maxNumbers = comparesion(firstNumbers, secondNumbers, thirdNumbers);
+        System.out.println("Maximum value is: " + maxNumbers);
+        System.out.println(" ");
+        //string value from user
+        System.out.println("enter the String:");
+        String firstString = scanner.next();
+        String secondString = scanner.next();
+        String thirdString = scanner.next();
+
+        String maxString = comparesion(firstString,secondString , thirdString);
+        System.out.println("Maximum value is:" + maxString);
+        scanner.close();
     }
 }
